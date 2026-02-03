543495586522eaa5438aa426a8fd8ee44873206c
Donglai Dai <donglai.dai@amd.com>
2026-02-03 03:41:10 -0500

    Add files for building XIO using docker. Redo due to project name change.

--- docker_build.sh
+++ docker_build.sh
@@ -1,11 +1,23 @@
 #! /bin/bash
 
-docker build -t axiio-build-image:v1  -f docker/Dockerfile .
+set -ex
+
+docker build -t xio-build-image:v1  -f docker/Dockerfile .
  
-# docker run --rm axiio-build-image:v1 find /usr -name "CLI.hpp"
+# docker run --rm xio-build-image:v1 find /usr -name "CLI.hpp"
+# docker run --rm xio-build-image:v1 cat /usr/include/CLI/CLI.hpp 
+# docker run --rm xio-build-image:v1 pwd; ls; cd src/include; pwd ; cd ../.. 
+docker run --rm xio-build-image:v1 \
+    sh -c "mkdir -p /home/AMD/dondai/rocm-xio.git/src && \
+           ln -s /usr/include/CLI /home/AMD/dondai/rocm-xio.git/src/include"
+docker run --rm xio-build-image:v1 which cmake
 
 docker run -it --rm   --device=/dev/kfd   --device=/dev/dri \
-      	axiio-build-image:v1 \
-	pwd; \
-	make all
+	-v $(pwd):/home/AMD/dondai/rocm-xio.git \
+	-w /home/AMD/dondai/rocm-xio.git \
+       	xio-build-image:v1 \
+	sh -c "mkdir -p build && cd build && cmake .. && cmake --build . --target all"
+
+set +x
+echo "Build Complete!"
 
